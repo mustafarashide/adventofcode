a3ae59e8430011ce0c7481f4e1a1b5ec52f154b2
mustafarashide <melhassan@haverford.edu>
2025-01-09 22:30:35 -0500

    finished part one day 2

--- src/Day02.java
+++ src/Day02.java
@@ -1,12 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Day02 {
-    public static int reportsReader() {
+    public static ArrayList<ArrayList<Integer>> reportsReader() {
         int safeReports = 0;
         ArrayList<ArrayList<Integer>> reports = new ArrayList<>();
         String regex = "[\\s]";
@@ -26,16 +26,51 @@ public class Day02 {
             System.out.println("An error occured");
             e.printStackTrace();
         }
-        //stuck here, i do not know what the clever way of doing this is but i divided into two cases and i am trying to check if going from one integer in the report
-        //to the next is legal in each case i.e. decreasing/increasing
-        for (ArrayList<Integer> line : reports) {
-            if (line.get(0) > line.get(1)) {//if it is decreasing
-            } else if (line.get(0) < line.get(1)) {//if it is increasing
+        return reports;
+    }
+    public static boolean safeReports(ArrayList<Integer> report) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i: report) {
+            set.add(i);
+        }
+        if(set.size() != report.size()) {
+            return false;//if there is a duplicate, then for sure this is not a safe report
+        }
+        //check if increasing/decreasing
+        //if increasing check if sorted report is unequal to report
+        //if decreasing check if reversed sorted report is unequal to report
+        if(report.get(0)<report.get(1)) {
+            ArrayList<Integer> sorted = new ArrayList<>(report);
+            Collections.sort(sorted);
+            if(!sorted.equals(report)) {
+                return false;
+            };
+        }
+        if(report.get(0)>report.get(1)) {
+            ArrayList<Integer> reversedSorted = new ArrayList<>(report);
+            Collections.sort(reversedSorted);
+            Collections.reverse(reversedSorted);
+            if(!reversedSorted.equals(report)) {
+                return false;
             }
         }
-        return safeReports;
-    }
+        for(int i=0;i<report.size()-1;i++) {
+            if(Math.abs(report.get(i)-report.get(i+1))!=1&&Math.abs(report.get(i)-report.get(i+1))!=2&&Math.abs(report.get(i)-report.get(i+1))!=3) {
+                return false;
+            }
+        }
+        return true;
+        }
     public static void main(String[] args) {
-        System.out.println(reportsReader());
+        reportsReader();
+        int safeReportsCount = 0;
+        ArrayList<ArrayList<Integer>> reports = reportsReader();
+        for(ArrayList<Integer> report: reports) {
+            if(safeReports(report)){
+                safeReportsCount++;
+            }
+        }
+        System.out.println(safeReportsCount);
     }
-}
+    }
+
