7d647e7af86796d467faf25f456b6db572ce5d49
mustafarashide <melhassan@haverford.edu>
2025-03-12 02:38:32 -0400

    bruh regex

--- src/Day03.java
+++ src/Day03.java
@@ -1,13 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 import java.util.regex.*;
 
 public class Day03 {
-    static StringBuilder input = new StringBuilder();
+
     public static String reader() throws FileNotFoundException {
+        StringBuilder input = new StringBuilder();
         try {
             File myObj = new File("inputs/input03.txt");
             Scanner myReader = new Scanner(myObj);
@@ -20,17 +19,38 @@ public class Day03 {
         }
         return input.toString();
     }
-    public static int result() throws FileNotFoundException {
-        String reader = reader();
+    public static int partOne(String input) throws FileNotFoundException {
         int result = 0;
         Pattern multiplications = Pattern.compile("mul\\(\\d*,\\d*\\)");
-        Matcher matcher = multiplications.matcher(reader);
+        Matcher matcher = multiplications.matcher(input);
         while (matcher.find()) {
-            System.out.println(matcher.group());
+            Integer i = Integer.valueOf(matcher.group().split(",")[0].replaceAll("[^0-9]", ""));
+            Integer j = Integer.valueOf(matcher.group().split(",")[1].replaceAll("[^0-9]", ""));//i know i may not need this but just in case
+            result += i * j;
         }
         return result;
     }
+    public static int partTwo(String input) throws FileNotFoundException {
+        int total= 0;
+        boolean isActive=true;
+        Pattern illegalMultiplications = Pattern.compile("mul\\(\\d*,\\d*\\)|don't\\(\\)|do\\(\\)");
+        Matcher illegalMatcher = illegalMultiplications.matcher(input);
+        while (illegalMatcher.find()) {
+            if(illegalMatcher.group().equals("don't()")) {isActive=false;}
+            if(illegalMatcher.group().equals("do()")) {isActive=true;}
+            System.out.println(illegalMatcher.group());
+            if(!illegalMatcher.group().equals("don't()")&&!illegalMatcher.group().equals("do()")){
+                Integer i = Integer.valueOf(illegalMatcher.group().split(",")[0].replaceAll("[^0-9]", ""));
+                Integer j = Integer.valueOf(illegalMatcher.group().split(",")[1].replaceAll("[^0-9]", ""));
+                if (isActive) {
+                    total += i * j;
+                }
+            }
+        }
+        return total;
+    }
     public static void main(String[] args) throws FileNotFoundException {
-        result();
+        int b= partTwo(reader());
+        System.out.println(b);
     }
 }
